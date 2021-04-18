6a68026a447883780e12ca2918e0e4da0e7561b1
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-04-18 15:32:00 -0400

    Add basic http understanding

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
 const http = require('http')
-
+const fs = require('fs')
 const server = http.createServer((req, res) => {
-  
-    console.log(req.url, req.method, req.headers)
-  //  process.exit()
-  res.setHeader('Context-Type', 'text/html')
-  res.write('<html>')
-  res.write('<header><title>  test </title></header>')
-  res.write('<body><h1>Sample test </h1>')
-  
-  res.write('</body>')
-  res.write('</html>')
-  res.end()
+
+    //  console.log(req.url, req.method, req.headers)
+    //  process.exit()
+    const url = req.url
+    const method = req.method
+    if (url === '/') {
+        res.setHeader('Context-Type', 'text/html')
+        res.write('<html>')
+        res.write('<header><title> express rest </title></header>')
+        res.write('<body><h1>')
+        res.write('<form action="/message" method="POST"><input type= "text" name="message"><button type= "submit">Send</button>')
+
+        res.write('</body>')
+        res.write('</html>')
+        res.statusCode = 200
+        return res.end()
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = []
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString()
+            console.log(parseBody)
+            const userMessage = parseBody.split('=')
+            console.log(userMessage[1])
+            fs.writeFileSync('message.text', userMessage)
+            res.statusCode = 302
+            res.setHeader('Location', '/')
+            res.end()
+        })
+        /*  res.setHeader('Context-Type', 'text/html')
+         res.write('<html>')
+         res.write('<header><title> express rest </title></header>')
+         res.write('<body><h1>')
+         res.write('<form action="/message" method="POST"><input type= "text" name="message"><button type= "submit">Send</button>')
+         
+         res.write('</body>')
+         res.write('</html>')
+      return  res.end() */
+    }
 })
 
 server.listen(3000)
