ac3ac2cb3aac7cbd14c921804bd9c85dc7cda8a5
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-04-20 23:16:49 -0400

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
-
+// Add a two routes 1. Load html page with form field 
+// Send form data to 
 const routeHandler = (req, res) => {
 const url = req.url
 const method = req.method
@@ -7,19 +8,34 @@ if(url === '/') {
     res.write('<html>')
     res.write('<title> Hello </title>')
     res.write('<body>')
-    res.write('<h1>Hello Test 1</h1>')
+    res.write('<h1>Enter a user name</h1>')
+    res.write('<form action="/user" method="POST"><input type= "text" name="message"><button type= "submit">Send</button>')
     res.write('</body>')
     res.write('</html>')
+    res.statusCode = 200
     res.end()
 }
-if(url=== '/user'){
-    res.setHeader('Context-Type', 'text/html')
-    res.write('<html>')
-    res.write('<body>')
-    res.write('<h1>Users List</h1>')
-    res.write('</body>')
-    res.write('</html>')
-    res.end()
+if(url=== '/user' && method === 'POST'){
+    const body = []
+    req.on('data', (chunk) => {
+        console.log(chunk)
+        body.push(chunk)
+    })
+    return req.on('end', () => {
+        const parseBody = Buffer.concat(body).toString
+        const userName = parseBody.split('=')
+        console.log(userName)
+        res.setHeader('Context-Type', 'text/html')
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<h1>User name list </h1>')
+        res.write('<li>')
+        res.write(`${userName}`)
+        res.write('</li>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end()
+    })
 }
 }
 
