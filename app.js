e1786ca98c8fb84ec8d6ff7e1ae146d1aa20d174
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-04-25 12:55:07 -0400

    Add body parser

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((req,res, next) => {
-    res.send('<h1>Hell express</h1>')
-    console.log(req.url)
+app.use(bodyParser.urlencoded({extended: false}))
+const products = []
+app.get('/add-products',(req,res, next) => {
+    res.send('<h1>Add list</h1><form action="/product" method ="POST"><input type ="text" name= "name"><button type="submit">Add product</button></form>')
+  
 })
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+products.push(req.body)
+    res.redirect('/list')
+})
+
+app.get('/list',(req, res, next) => {
+    res.send('<h1> List of products</h1>')
+
+})
+
 
 app.listen(3000) 
