507ef3934c8dc5071537e6c17621fe4210979e73
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-04-27 23:32:11 -0400

    Fix 404 error after clicking add product

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const shopRoute =  require('./routes/shop')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
-app.use('/admin',adminRoute)
+app.use(adminRoute)
 // When both method are having a common path we can apply a filters 
 // EX. adminRoute are having different methods but we can apply filter 
 app.use(shopRoute)
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const path = require('path')
-
-router.get('/admin/add-product',(req,res, next) => {
+const rootDir = require('../util/path')
+router.get('/add-product',(req,res, next) => {
   // res.send(('<form action="/admin/add-product" method="POST"><input type= "text" name="message"><button type= "submit">Send</button>'))
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
   
 })
 
