7824cf12edc7a349d4056142c947b1b3278f67dc
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-05-07 22:34:41 -0400

    Update style

--- assignment4/app.js
+++ assignment4/app.js
@@ -13,7 +13,9 @@ app.use('/login', (req, res, next) => {
     res.sendFile(path.join(__dirname, './', 'views', 'index.html'))
   
 })
-
+app.use((req, res, next) => {
+    res.status(404).send('Page not found !!')
+})
 
  app.use(userList)
 app.listen(3000)
