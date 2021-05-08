59540affd707fa9fca8421752ca8aa61e8e10bf2
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-05-07 22:06:44 -0400

    Update routing

--- assignment4/app.js
+++ assignment4/app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const path = require('path')
 const router = express.Router()
+const bodyParser = require('body-parser')
 const userList = require('./router/users')
+const dirRoute = require('./helper/path')
 const app = express()
 
+app.use(bodyParser.urlencoded({extended : false}))
 
-app.use('/', (req, res, next) => {
+app.use('/login', (req, res, next) => {
 
-   res.sendFile(path.join(__dirname, './', 'views', 'index.html'))
+    res.sendFile(path.join(__dirname, './', 'views', 'index.html'))
   
 })
-app.use(userList)
+
+
+ app.use(userList)
 app.listen(3000)
--- assignment4/router/users.js
+++ assignment4/router/users.js
@@ -2,14 +2,17 @@ const express = require('express')
 const router = express.Router()
 const dirRoute = require('../helper/path')
  const path = require('path')
-/* 
- router.get('/users', (req, res, next) => {
-    res.sendFile(path.join(dirRoute, 'views', 'users.html'))
- }) */
-router.post('/users', (req, res, next) => {
-console.log(req.body)
-    res.redirect('/')
+
+ router.post('/users', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/users-list')
     
-})
+ })
+ 
+ router.get('/users-list', (req, res, next) => {
+    res.sendFile(path.join(dirRoute, 'views', 'users.html'))
+   
+    
+ })
 
 module.exports= router
