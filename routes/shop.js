a9ef5070b8220eee1dcef86d2e53aab7c9a676ac
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-05-08 22:10:54 -0400

    Add basic pug html template engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const path = require('path')
+const router = express.Router()
+
 const rootDir = require('../util/path')
 const adminRoute = require('./admin')
-const router = express.Router()
+const product = require('../routes/admin')
 router.get('/',(req, res, next) => {
-   
-    res.render('shop')
+   const productName = product.product[0].productTitle
+  
+    res.render('shop', {pName: productName})
+    console.log(productName)
    // res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'))
     
 
