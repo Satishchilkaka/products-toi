d1c72bb206d2fab3a668021b73d020c925bd5caf
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-05-20 22:32:51 -0400

    Update shop route

--- routes/shop.js
+++ routes/shop.js
@@ -5,15 +5,26 @@ const router = express.Router()
 const rootDir = require('../util/path')
 const adminRoute = require('./admin')
 const product = require('../routes/admin')
-router.get('/',(req, res, next) => {
+router.get('/', (req, res, next) => {
    const productName = product.product
-  // const productName = product.product[0].productTitle
-    res.render('shop', {productTitle: product.product[0], path: '/', pageTitle: 'shop', hasProducts: product.product.length > 0})
-    console.log(productName)
+
+   res.render('shop', {
+      productTitle: product.product[0], 
+      path: '/', 
+      pageTitle: 'shop', 
+      hasProducts: product.product.length > 0,
+      activeShop: true,
+      productCSS: true
+
+   })
+})
+
+   // const productName = product.product[0].productTitle
+   // console.log(productName)
    // res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'))
    // console.log(product.product.productTitle)
 
-})
+
 
 
 module.exports = router
