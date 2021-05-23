edc55a11c15e86767e1ae23c451146157a274a58
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-05-23 18:13:30 -0400

    Add controller

---
+++ controllers/products.js
@@ -0,0 +1,30 @@
+const product = []
+exports.addProductsToShop = (req, res, next) => {
+    res.render('add-product', {
+        pageTitle: 'Add products to shop',
+        path: '/admin/add-product',
+        activeAddProduct: true,
+        productCSS: true,
+        formsCSS: true
+    })
+}
+
+exports.getProducts = (req, res, next) => {
+
+    const AdminAddProduct = JSON.parse(JSON.stringify(req.body))
+
+    product.push({ productTitle: AdminAddProduct.title })
+    res.redirect('/')
+}
+
+exports.displayProductInStore = (req, res, next) => {
+
+    res.render('shop', {
+        productName: product,
+        path: '/',
+        pageTitle: 'shop',
+        hasProducts: product.length > 0,
+        activeShop: true,
+        productCSS: true
+    })
+}
