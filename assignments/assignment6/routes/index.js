2e98710248f79b3a02648ab02c0c4f86f0534c09
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-06-06 22:10:46 -0400

    Add password field

--- assignments/assignment6/routes/index.js
+++ assignments/assignment6/routes/index.js
@@ -2,11 +2,30 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const rootDir = require('../util/path')
-router.get('/', (req, res, next) => {
-    res.status(200).render('index', {
-        pageTitle: 'index',
-        path: '/index'
-    })
+const bodyParser = require('body-parser')
+
+
+
+ router.post('/user-welcome', (req, res, next) => {
+    const name = JSON.parse(JSON.stringify(req.body))
+     res.status(200)
+     .render('user-welcome', {
+         path: '/user-welcome',
+         pageTitle: 'User profile'
+     })
+
+    
+    
+     console.log(name)
+
  })
 
+ router.get('/user-welcome', (req, res, next) => {
+    res.status(200)
+    .render('user-welcome', {
+        path:'/user-welcome',
+        pageTitle: 'User profile'
+
+    })
+})
 module.exports = router
