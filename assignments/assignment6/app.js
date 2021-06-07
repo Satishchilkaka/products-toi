0138faf362955a742d49e04c58ec7d0026f4aad3
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-06-06 22:10:02 -0400

    Move index to main file

--- assignments/assignment6/app.js
+++ assignments/assignment6/app.js
@@ -4,6 +4,12 @@ const indexRoute = require('./routes/index')
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
+app.get('/', (req, res, next) => {
+    res.status(200).render('index', {
+        pageTitle: 'index',
+        path: '/index'
+    })
+ })
 app.use(indexRoute)
 app.listen(3000)
 
