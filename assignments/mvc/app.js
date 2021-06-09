bedffcb6ee7ce598d8e31453b729ef73026604ea
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-06-08 23:13:25 -0400

    Update the app.js

--- assignments/mvc/app.js
+++ assignments/mvc/app.js
@@ -1,6 +1,13 @@
 const express = require ('express')
 const app = express()
 
+const loginRoute = require('./routes/login')
+const userRoute = require('./routes/userProfile')
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
 
+app.use(loginRoute)
+app.use(userRoute)
 app.listen(3000)
