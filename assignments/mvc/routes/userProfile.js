d2d165dcf3cf6fcc8ba3f041c89fb79c693d5136
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-06-09 23:22:56 -0400

    Add user data to array

--- assignments/mvc/routes/userProfile.js
+++ assignments/mvc/routes/userProfile.js
@@ -1,21 +1,27 @@
 const express = require('express')
 const router = express.Router()
-
-
+const bodyParser = require('body-parser')
+const { stringifyConfiguration } = require('tslint/lib/configuration')
+const userData = []
+router.use(bodyParser.urlencoded({extended: false}))
 router.get('/users-list', (req, res, next) => {
     res.status(200)
     .render('users-list', {
         path: '/users-list',
-        pageTitle:'user profile'
+        pageTitle:'user profile',
+       
     })
 })
 
 router.post('/users-list', (req, res, next) => {
+   
     res.status(200)
     .render('users-list', {
         path: '/users-list',
         pgeTitle: 'use'
     })
+    const userD = JSON.parse(JSON.stringify(req.body))
+    console.log(userD)
 })
 
 
