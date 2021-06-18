a51bda17055d1b80a762635ea3ffb47bd34ade9f
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-06-17 23:02:57 -0400

    Update admin and shop controllers with new orders admin products and cart

---
+++ controllers/shop.js
@@ -0,0 +1,44 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      productName: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      productName: products,
+      pageTitle: 'Shop',
+      path: '/shop/index',
+      formsCSS: true,
+      productCSS: true
+    });
+  });
+};
+
+exports.getCartView = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/shop/cart',
+    pageTitle: 'Cart',
+    formsCSS: true,
+    productCSS: true
+    
+  });
+};
+
+exports.getOrdersView = (req, res, next) => {
+  res.status(200)
+  .render('shop/orders', {
+    path: '/shop/orders',
+    pageTitle: 'Orders',
+    mainCSS: true,
+    productCSS: true
+  })
+}
+
