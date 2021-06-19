103f7d592d4b8a25926fcbb61d77e339a5104933
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-06-18 21:07:01 -0400

    Update app

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const path = require('path');
 
 const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error');
+const errorHandle = require('./controllers/error');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const shopAdminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', shopAdminRoute);
+app.use(shopRoute);
 
-app.use(errorController.notFound);
+app.use(errorHandle.notFound);
 
 app.listen(3000);
